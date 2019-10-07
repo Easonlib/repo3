96f707ecf61e17a94ae0ea2a63aef30454609a2e
Easonlib <51535731+Easonlib@users.noreply.github.com>
2019-10-07 22:16:38 +0800

    main方法

---
+++ HelloProjet/src/Hello.java
@@ -0,0 +1,13 @@
+package trunk.HelloProjet.src;
+
+/**
+ * @author XiangQiang
+ * @version 1.0
+ * @date 2019.10.07 22:15
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+
+    }
+}
